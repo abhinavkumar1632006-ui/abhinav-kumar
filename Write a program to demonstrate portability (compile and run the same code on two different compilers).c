9a081555815dcb85c7b7ec33495b3568d53f220a
abhinavkumar1632006-ui <abhinavkumar1632006@gmail.com>
2025-08-28 14:25:35 +0530

    Create Write a program to demonstrate portability (compile and run the same code on two different compilers).c

---
+++ Write a program to demonstrate portability (compile and run the same code on two different compilers).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+// Function to demonstrate portabilit {
+   printf("This program demonstrates portability in C.\n");
+    int a = 10, b = 20, sum;
+    sum = a + b;
+        printf("The sum of %d and %d is %d\n", a, b, sum);
+
+    return 0; // Returning 0 indicates successful execution
+}
