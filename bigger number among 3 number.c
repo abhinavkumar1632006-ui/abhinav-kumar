4007d43792c3e858cdebf82cf89b9d893828fbe2
abhinavkumar1632006-ui <abhinavkumar1632006@gmail.com>
2025-08-28 16:16:58 +0530

    Create bigger number among 3 number.c

---
+++ bigger number among 3 number.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+
+int main() {
+    int a,b,c;
+    printf("ENTER THREE NUMBERS\n");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b && a>c)
+    
+        printf("bigger number is %d\n",a);
+            
+            else if(b>a && b>c)
+            
+               printf("bigger number is %d\n",b);  
+            
+else
+
+     printf("bigger number is %d\n",c);
+
+}
